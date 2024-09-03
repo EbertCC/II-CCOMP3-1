a5e2387bb93a24bf688442edcc67fcb401dcbb64
EbertCC <165855506+EbertCC@users.noreply.github.com>
2024-09-02 20:47:42 -0500

    Add files via upload

---
+++ ejercicio_2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void fun(double num){
+    double indi[] = {200,100,50,20,10,5,2,1,0.5,0.2,0.1} ;
+    
+    for(int i = 0 ; i < 11 ; i++){
+        num = round(num*10)/10.0;
+        int aux = num/indi[i];
+        num = num - aux*indi[i];
+        cout<<indi[i]<<" -> "<<aux<<endl;
+    }
+}
+
+int main(){
+    double  num;
+    cout<<"Ingrese numero flotante"<<endl;
+    cin>>num;
+    fun(num);
+
+    return 0;
+}
